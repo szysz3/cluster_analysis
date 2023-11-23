121a04cd6071e26fe4634fd3b9af8ac4ac8af1ad
szysz3 <michalszyszka.m@gmail.com>
2023-11-23 19:15:34 +0100

    Start PCA implementation

--- lib/data_processing/principal_component_analysis.dart
+++ lib/data_processing/principal_component_analysis.dart
@@ -1,14 +1,28 @@
 import 'package:cluster_analysis/common/model/abstract_data_item.dart';
+import 'package:cluster_analysis/data_processing/tools.dart';
 
 class PrincipalComponentAnalysis {
   void calculate(List<AbstractDataItem> dataItems) {
     // TODO:
+
     // Input: matrix with data items
     // Output: reduced mantrix with data items
-    //
+
     // 1. Calculate means for every row in matrix
+    var meanValuesList = List.empty(growable: true);
+    for (var dataRow in dataItems) {
+      meanValuesList.add(calculateMeanValue(dataRow.values));
+    }
+
     // 2. From every variable in row subtract mean of the whole row
+    for (var i = 0; i < dataItems.length; i++) {
+      for (var j = 0; j < dataItems[i].values.length; j++) {
+        dataItems[i].values[j] /= meanValuesList[j];
+      }
+    }
+
     // 3. Calculate covariance matrix
+
     // 4. Calculate eigen vectors and values of covariance matrix
     // 5. Rearrange eigen vectors and eigen values
     // 6. Choose principal components
---
+++ test/data_processing/principal_component_analysis_test.dart
@@ -0,0 +1,20 @@
+import 'package:cluster_analysis/common/model/abstract_data_item.dart';
+import 'package:cluster_analysis/data_processing/principal_component_analysis.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('standarize data with z-score', () async {
+    //arrange
+    var data = List<AbstractDataItem>.of([
+      AbstractDataItem(values: List.of([1, 1])),
+      AbstractDataItem(values: List.of([3, 3])),
+      AbstractDataItem(values: List.of([10, 55])),
+    ]);
+
+    // act
+    var pca = PrincipalComponentAnalysis();
+    pca.calculate(data);
+
+    // assert
+  });
+}
