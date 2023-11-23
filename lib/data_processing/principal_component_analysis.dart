ea8452dad666ce4f7d73201ab8d188bac846cff0
szysz3 <michalszyszka.m@gmail.com>
2023-11-23 18:52:40 +0100

    Add todos in pca class

--- lib/data_processing/principal_component_analysis.dart
+++ lib/data_processing/principal_component_analysis.dart
@@ -3,6 +3,15 @@ import 'package:cluster_analysis/common/model/abstract_data_item.dart';
 class PrincipalComponentAnalysis {
   void calculate(List<AbstractDataItem> dataItems) {
     // TODO:
+    // Input: matrix with data items
+    // Output: reduced mantrix with data items
     //
+    // 1. Calculate means for every row in matrix
+    // 2. From every variable in row subtract mean of the whole row
+    // 3. Calculate covariance matrix
+    // 4. Calculate eigen vectors and values of covariance matrix
+    // 5. Rearrange eigen vectors and eigen values
+    // 6. Choose principal components
+    // 7. Project data
   }
 }
