ed285596dc4ea04889feda4e50cf6b1e13e427b7
Michal Szyszka <michalszyszka.m@gmail.com>
2024-02-10 13:46:49 +0100

    Extend exported classes

--- lib/cluster_analysis.dart
+++ lib/cluster_analysis.dart
@@ -3,7 +3,15 @@ library cluster_analysis;
 // DBSCAN
 export 'package:cluster_analysis/algorithms/dbscan/dbscan.dart';
 export 'package:cluster_analysis/algorithms/dbscan/model/data_item.dart';
+export 'package:cluster_analysis/algorithms/dbscan/model/dbscan_config.dart';
 // k-means
 export 'package:cluster_analysis/algorithms/k_means/k_means.dart';
 export 'package:cluster_analysis/algorithms/k_means/model/cluster.dart';
 export 'package:cluster_analysis/algorithms/k_means/model/k_means_config.dart';
+// data processing
+export 'package:cluster_analysis/data_processing/data_import.dart';
+export 'package:cluster_analysis/data_processing/principal_component_analysis.dart';
+export 'package:cluster_analysis/data_processing/z_score_normalization.dart';
+// common
+export 'package:cluster_analysis/common/model/abstract_cluster.dart';
+export 'package:cluster_analysis/common/similarity_measure/squared_euclidean_distance.dart';
